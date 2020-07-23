418d3645f9923ae36969e2a04a246806bddb7d37
Florian Mötz <FlowMo7@users.noreply.github.com>
2020-07-23 17:57:22 +0200

    Add java-diff-utils to improve output of check task (which now actually prints the found differences)

--- src/main/kotlin/com/tailoredapps/gradle/localize/Localize.kt
+++ src/main/kotlin/com/tailoredapps/gradle/localize/Localize.kt
@@ -5,6 +5,7 @@ import com.tailoredapps.gradle.localize.android.ParsedSheetToAndroidTransformer
 import com.tailoredapps.gradle.localize.drive.DriveManager
 import com.tailoredapps.gradle.localize.localization.LocalizationSheetParser
 import com.tailoredapps.gradle.localize.util.forEachParallel
+import difflib.DiffUtils
 import java.io.File
 import java.io.IOException
 
@@ -12,7 +13,8 @@ class Localize {
 
     private val driveManager: DriveManager = DriveManager()
     private val localizationSheetParser: LocalizationSheetParser = LocalizationSheetParser()
-    private val androidSheetTransformer: ParsedSheetToAndroidTransformer = ParsedSheetToAndroidTransformer()
+    private val androidSheetTransformer: ParsedSheetToAndroidTransformer =
+        ParsedSheetToAndroidTransformer()
     private val stringXmlGenerator: AndroidStringXmlGenerator = AndroidStringXmlGenerator()
 
     suspend fun localize(config: LocalizationConfig) {
@@ -55,34 +57,70 @@ class Localize {
             languageColumnTitles = config.languageTitles
         )
 
-        val localizationsWithDifferences = mutableListOf<String>()
+        val diffs = mutableListOf<String>()
 
-        config.languageTitles.forEachParallel { language ->
-            val transformedSheet = androidSheetTransformer.transformForLanguage(
-                language = language,
-                parsedSheet = parsedSheet
-            )
-            val stringXmlContent = stringXmlGenerator.androidValuesToStringsXml(
-                values = transformedSheet,
-                addComments = config.addComments
-            )
+        config.languageTitles
+            .map { language ->
+                val transformedSheet = androidSheetTransformer.transformForLanguage(
+                    language = language,
+                    parsedSheet = parsedSheet
+                )
+                val stringXmlContent = stringXmlGenerator.androidValuesToStringsXml(
+                    values = transformedSheet,
+                    addComments = config.addComments
+                )
 
-            val file = getStringsXmlFileOrThrow(
-                localizationPath = config.localizationPath,
-                localizationIdentifier = if (language == config.baseLanguage) null else language
-            )
+                val file = getStringsXmlFileOrThrow(
+                    localizationPath = config.localizationPath,
+                    localizationIdentifier = if (language == config.baseLanguage) null else language
+                )
 
-            if (stringXmlContent != file.readText()) {
-                localizationsWithDifferences.add(language)
+
+                val diffResult = compareFiles(
+                    expectedFileName = config.localizationPath.canonicalPath + " (remote)",
+                    expectedFileContent = stringXmlContent,
+                    actualFileName = config.localizationPath.canonicalPath + " (local)",
+                    actualFileContent = file.readText()
+                )
+
+
+                if (diffResult != null) {
+                    diffs.add(diffResult)
+                }
             }
-        }
 
-        if (localizationsWithDifferences.isNotEmpty()) {
-            throw RuntimeException("Localizations are not up-to-date. Found differences in languages: ${localizationsWithDifferences.joinToString()}")
+        if (diffs.isNotEmpty()) {
+            throw RuntimeException(
+                "Localizations are not up-to-date.\n\n${diffs.joinToString(
+                    separator = "\n"
+                )}"
+            )
         }
     }
 
-    private fun getStringsXmlFileOrThrow(localizationPath: File, localizationIdentifier: String?): File {
+
+    private fun compareFiles(
+        expectedFileName: String,
+        expectedFileContent: String,
+        actualFileName: String,
+        actualFileContent: String
+    ): String? {
+        // We don't compare full text because newlines on Windows & Linux/macOS are different
+        val checkLines = expectedFileContent.lines()
+        val actualLines = actualFileContent.lines()
+        if (checkLines == actualLines)
+            return null
+
+        val patch = DiffUtils.diff(checkLines, actualLines)
+        val diff =
+            DiffUtils.generateUnifiedDiff(expectedFileName, actualFileName, checkLines, patch, 3)
+        return diff.joinToString("\n")
+    }
+
+    private fun getStringsXmlFileOrThrow(
+        localizationPath: File,
+        localizationIdentifier: String?
+    ): File {
         val valuesDirectory = File(
             localizationPath,
             if (localizationIdentifier == null) "values" else "values-$localizationIdentifier"
